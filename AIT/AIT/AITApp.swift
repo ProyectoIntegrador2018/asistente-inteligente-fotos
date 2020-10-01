d3a95038225a42c313a453818854036f42684b14
Cristina De Leon <cristina.dlmtz@gmail.com>
2020-10-01 00:32:31 -0500

    Inicio de proyecto en XCode, versión 12.0.1

---
+++ AIT/AIT/AITApp.swift
@@ -0,0 +1,17 @@
+//
+//  AITApp.swift
+//  AIT
+//
+//  Created by Terneros
+//
+
+import SwiftUI
+
+@main
+struct AITApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
