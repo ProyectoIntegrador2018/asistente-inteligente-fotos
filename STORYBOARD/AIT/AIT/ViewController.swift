910a064cae7bc42224830062696853d4c0682696
MM46 <mm@MacBook-Pro-de-Marian.local>
2020-10-18 21:29:17 -0500

    PrepareSegue

--- STORYBOARD/AIT/AIT/ViewController.swift
+++ STORYBOARD/AIT/AIT/ViewController.swift
@@ -14,6 +14,18 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func llantasView(_ sender: Any) {
+        self.performSegue(withIdentifier: "llantasSegue", sender: self)
+        
+    }
+    
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//           if segue.identifier == "segueToNext" {
+//               if let destination = segue.destination as? Modo1ViewController {
+//                   destination.nomb = nombres // you can pass value to destination view controller
+//               }
+//           }
+    
+    
 }
 
