b10bbf646064a260ea91b31f5b594a673c8ab921
Kevin Radtke <kevinradtkeg@gmail.com>
2020-10-27 19:59:41 -0600

    feat: checks if image is blurred

--- STORYBOARD/AIT/AIT/llantasViewController.swift
+++ STORYBOARD/AIT/AIT/llantasViewController.swift
@@ -7,6 +7,9 @@
 
 import UIKit
 import AVFoundation
+import Metal
+import MetalPerformanceShaders
+import MetalKit
 
 
 class llantasViewController: UIViewController, AVCapturePhotoCaptureDelegate {
@@ -133,8 +136,17 @@ class llantasViewController: UIViewController, AVCapturePhotoCaptureDelegate {
             else { return }
         
         let image = UIImage(data: imageData)
-        UIImageWriteToSavedPhotosAlbum(image!, self, #selector(image(_:didFinishSavingWithError:contextInfo:)), nil)
-        photoView.image = image
+        let sharpness = calcSharpness(img: image!)
+        print(sharpness)
+        if (sharpness > 1) {
+            UIImageWriteToSavedPhotosAlbum(image!, self, #selector(image(_:didFinishSavingWithError:contextInfo:)), nil)
+            photoView.image = image
+        }
+        else {
+            let ac = UIAlertController(title: "Error", message: "La imagen esta borrosa. Por favor vuelva a intentarlo.", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+        }
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -225,4 +237,45 @@ class llantasViewController: UIViewController, AVCapturePhotoCaptureDelegate {
     }
 }
 
-
+func calcSharpness(img: UIImage) -> Int8 {
+    let mtlDevice = MTLCreateSystemDefaultDevice()
+    let mtlCommandQueue = mtlDevice?.makeCommandQueue()
+    
+    // Create a command buffer for the transformation pipeline
+    let commandBuffer = mtlCommandQueue!.makeCommandBuffer()!
+    // These are the two built-in shaders we will use
+    let laplacian = MPSImageLaplacian(device: mtlDevice!)
+    let meanAndVariance = MPSImageStatisticsMeanAndVariance(device: mtlDevice!)
+    
+    // Load the captured pixel buffer as a texture
+    let textureLoader = MTKTextureLoader(device: mtlDevice!)
+    let sourceTexture = try! textureLoader.newTexture(cgImage: img.cgImage!, options: nil)
+    
+    // Create the destination texture for the laplacian transformation
+    let lapDesc = MTLTextureDescriptor.texture2DDescriptor(pixelFormat: sourceTexture.pixelFormat, width: sourceTexture.width, height: sourceTexture.height, mipmapped: false)
+    lapDesc.usage = [.shaderWrite, .shaderRead]
+    let lapTex = mtlDevice!.makeTexture(descriptor: lapDesc)!
+    
+    // Encode this as the first transformation to perform
+    laplacian.encode(commandBuffer: commandBuffer, sourceTexture: sourceTexture, destinationTexture: lapTex)
+    
+    // Create the destination texture for storing the variance.
+    let varianceTextureDescriptor = MTLTextureDescriptor.texture2DDescriptor(pixelFormat: sourceTexture.pixelFormat, width: 2, height: 1, mipmapped: false)
+    varianceTextureDescriptor.usage = [.shaderWrite, .shaderRead]
+    let varianceTexture = mtlDevice!.makeTexture(descriptor: varianceTextureDescriptor)!
+    
+    // Encode this as the second transformation
+    meanAndVariance.encode(commandBuffer: commandBuffer, sourceTexture: lapTex, destinationTexture: varianceTexture)
+    
+    // Run the command buffer on the GPU and wait for the results
+    commandBuffer.commit()
+    commandBuffer.waitUntilCompleted()
+    
+    // The output will be just 2 pixels, one with the mean, the other the variance.
+    var result = [Int8](repeatElement(0, count: 2))
+    let region = MTLRegionMake2D(0, 0, 2, 1)
+    varianceTexture.getBytes(&result, bytesPerRow: 1 * 2 * 4, from: region, mipmapLevel: 0)
+    
+    let variance = result.last!
+    return variance
+}
